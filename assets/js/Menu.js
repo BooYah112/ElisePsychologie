59ff60650597db9d6eceb42c2953e9eda2867b01
DU PELOUX Flavien <FDX1@dsone.3ds.com>
2018-07-13 16:29:04 +0200

    All page (including menu and footer) are within a container

--- assets/js/Menu.js
+++ assets/js/Menu.js
@@ -6,21 +6,34 @@ $(function(){
     var callback = undefined;
     if (source === 'menuBar'){
       callback = function(){
-        $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-          if (!$(this).next().hasClass('show')) {
-            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-          }
-          var $subMenu = $(this).next(".dropdown-menu");
-          $subMenu.toggleClass('show');
+        var $previousSubMenu = undefined;
+        var hoverIn = function(e){
+            if (!$(this).next().hasClass('show')) {
+              $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+            }
+            var $subMenu = $(this).next(".dropdown-menu");
+            if ($previousSubMenu && $previousSubMenu.is($subMenu)){
+              // console.log("sub menu is same as previous. keep it");
+            } else{
+              $subMenu.toggleClass('show');
+            }
 
 
-          $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-            $('.dropdown-submenu .show').removeClass("show");
-          });
+            $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+              $('.dropdown-submenu .show').removeClass("show");
+            });
 
 
-          return false;
-        });
+            return false;
+        };
+        var hoverOut = function(e){
+          $previousSubMenu = $(e.target).next(".dropdown-menu");
+          var target = e.target;
+          // console.log(target);
+          // console.log(target.classList);
+          // console.log("hover Out");
+        }
+        $('.dropdown-menu a.dropdown-toggle').hover(hoverIn,hoverOut);
       };
     }
     $(this).load(file, callback);
